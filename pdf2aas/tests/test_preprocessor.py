fb83f928334271b7273c3e7cbf605a2b51d61c6b
Julian Grothoff <julian.grothoff@sms-group.com>
2024-02-19 10:38:27 +0000

    test(pre): add test for pdf2htmlEX html output

--- pdf2aas/tests/test_preprocessor.py
+++ pdf2aas/tests/test_preprocessor.py
@@ -1,5 +1,12 @@
-from preprocessor import DummyPDF2HTML
+from preprocessor import DummyPDF2HTML, PDF2HTMLEX
 
 def test_dummy_pdf_2_html_convert():
     p = DummyPDF2HTML()
     assert p.convert([]) == "html"
+
+def test_pdf2htmlEX_convert():
+    p = PDF2HTMLEX()
+    html_converted = p.convert("tests/assets/dummy-test-datasheet.pdf")
+    with open('tests/assets/dummy-test-datasheet.html') as html_file:
+        html_expected = html_file.read()
+        assert(html_converted == html_expected)
