38c88264ab8d117b8035b462298b0b5d2b329afc
Julian Grothoff <julian.grothoff@sms-group.com>
2024-02-19 10:37:27 +0000

    feat(pre): add pdf2htmlEX call and return converted html

--- pdf2aas/preprocessor/pdf2htmlEX.py
+++ pdf2aas/preprocessor/pdf2htmlEX.py
@@ -1,7 +1,31 @@
-from core import Preprocessor
+from preprocessor.core import Preprocessor
+from pathlib import Path
 import subprocess
-subprocess.call("pdf2htmlEX /path/to/foobar.pdf", shell=True)
 
-class PDF2HTMLEX():
-    def convert(self, filepath: str) -> str:
-        return "html"
+class PDF2HTMLEX(Preprocessor):
+    temp_dir = "temp/html"
+
+    def convert(self, filepath: str) -> str | None:
+        filename = Path(filepath).stem
+        dest_dir = Path(self.temp_dir, filename)
+        pdf2htmlEX = subprocess.run(['pdf2htmlEX',
+            '--embed-css', '0',
+            '--embed-font', '0',
+            '--embed-image', '0',
+            '--embed-javascript', '0',
+            '--embed-outline', '0',
+            '--split-pages', '0',
+            '--process-nontext', '0',
+            '--process-outline', '0',
+            '--printing', '0',
+            '--embed-external-font', '0',
+            '--optimize-text', '1',
+            '--dest-dir', dest_dir,
+            filepath])
+        #TODO log stdout/stderr from subprocess
+
+        if pdf2htmlEX.returncode != 0:
+            print("Call to pdf2htmlEX failed:" + str(pdf2htmlEX))
+            return None
+        
+        return Path(dest_dir, filename + '.html').read_text()
