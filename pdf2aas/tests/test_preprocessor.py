4dbaaae0ed65b24621faed39ae57f2ed21050641
Julian Grothoff <julian.grothoff@sms-group.com>
2024-02-19 14:26:43 +0000

    test(pre): add test for reduction of pdf2htmlEX preprocessor

--- pdf2aas/tests/test_preprocessor.py
+++ pdf2aas/tests/test_preprocessor.py
@@ -1,12 +1,29 @@
-from preprocessor import DummyPDF2HTML, PDF2HTMLEX
+import pytest
+from preprocessor import DummyPDF2HTML, PDF2HTMLEX, ReductionLevel
 
 def test_dummy_pdf_2_html_convert():
     p = DummyPDF2HTML()
     assert p.convert([]) == "html"
 
-def test_pdf2htmlEX_convert():
-    p = PDF2HTMLEX()
-    html_converted = p.convert("tests/assets/dummy-test-datasheet.pdf")
-    with open('tests/assets/dummy-test-datasheet.html') as html_file:
-        html_expected = html_file.read()
-        assert(html_converted == html_expected)
+
+class TestPDF2HTMLEX:
+    preprocessor = PDF2HTMLEX()
+    datasheet_prefix = 'tests/assets/dummy-test-datasheet'
+
+    def dummy_datasheet_html(self):
+        with open(f'{self.datasheet_prefix}.html') as html_file:
+            return html_file.read()
+
+    def test_pdf2htmlEX_convert(self):
+        html_converted = self.preprocessor.convert(f'{self.datasheet_prefix}.pdf')
+        assert(html_converted == self.dummy_datasheet_html())
+
+    @pytest.mark.parametrize("reduction_level", [l for l in ReductionLevel])
+    def test_pdf2htmlEX_reduce(self, reduction_level):
+        html_reduced = self.preprocessor.reduce_datasheet(self.dummy_datasheet_html(), reduction_level)
+        if reduction_level >= ReductionLevel.PAGES:
+            assert(isinstance(html_reduced, list))
+            html_reduced = str.join('\n', html_reduced)
+        with open(f'{self.datasheet_prefix}_{reduction_level.value}_{reduction_level.name}.html') as html_file:
+            html_expected = html_file.read()
+            assert(html_reduced == html_expected)
