124ea20d32ecf0fff0aa3e9a93547ef67bf0f55f
Julian Grothoff <julian.grothoff@sms-group.com>
2024-06-10 12:14:31 +0200

    feat(dict): export class definition for dictionary module

--- src/pdf2aas/dictionary/__init__.py
+++ src/pdf2aas/dictionary/__init__.py
@@ -1,8 +1,9 @@
-from .core import PropertyDefinition, ETIM, DummyDictionary, dictionary_serializer
+from .core import PropertyDefinition, ClassDefinition, ETIM, DummyDictionary, dictionary_serializer
 from .eclass import ECLASS
 
 __all__ = [
     "PropertyDefinition",
+    "ClassDefinition",
     "ETIM",
     "DummyDictionary",
     "dictionary_serializer",
