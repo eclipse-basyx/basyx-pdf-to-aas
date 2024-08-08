7e388a6563069e0e02266bbe1b9af694b6deb596
Julian Vogel <julian.vogel@sms-group.com>
2024-08-08 13:05:25 +0200

    test: add test for csv generator

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,6 +1,23 @@
-from pdf2aas.generator import DummyTechnicalDataSubmodel
+from pdf2aas.generator import DummyTechnicalDataSubmodel, CSV
+from pdf2aas.extractor import DummyPropertyLLM
+from pdf2aas.dictionary import PropertyDefinition
 
 
 def test_dummy_technical_data_submodel_generate():
     g = DummyTechnicalDataSubmodel()
     assert g.generate([]) == "<AASSubmodel>DUMMY</AASSubmodel>"
+
+test_property_list = DummyPropertyLLM.empty_property_result(PropertyDefinition("id1", {'en': 'name1'})) + [{
+    "property": "property2",
+    "value": 1,
+    "unit": "kg",
+    "reference": "property1 is one kg",
+    "id": "id2",
+    "name": "name2"
+}]
+
+def test_csv_generate():
+    g = CSV()
+    csv = g.generate(test_property_list)
+    with(open('tests/assets/dummy-result.csv') as file):
+        assert csv == file.read()
