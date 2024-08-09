f1d0283ce50ce87f6f0b567b787641715ccd9d04
Julian Vogel <julian.vogel@sms-group.com>
2024-08-09 14:47:51 +0200

    feat(dict): add etim api authorization function

--- src/pdf2aas/dictionary/etim.py
+++ src/pdf2aas/dictionary/etim.py
@@ -1,4 +1,7 @@
+import os
 import logging
+import requests
+import time
 import re
 
 from .core import Dictionary, ClassDefinition, PropertyDefinition
@@ -29,11 +32,13 @@ class ETIM(Dictionary):
         scope: str = "EtimApi",
     ) -> None:
         super().__init__(release, temp_dir)
-        self.client_id = client_id
-        self.client_secret = client_secret
+        self.client_id = client_id if client_id is not None else os.environ.get("ETIM_CLIENT_ID")
+        self.client_secret = client_secret if client_secret is not None else os.environ.get("ETIM_CLIENT_SECRET")
         self.auth_url = auth_url
         self.base_url = base_url
         self.scope = scope
+        self.__access_token = None
+        self.__expire_time = None
     
     def get_class_properties(self, class_id: str) -> list[PropertyDefinition]:
         raise NotImplementedError()
@@ -47,6 +52,32 @@ class ETIM(Dictionary):
 
     def get_property_url(self, property_id: str) -> str:
         return f"https://prod.etim-international.com/Feature/Details/{property_id}"
+    
+    def __get_access_token(self) -> str:
+        if (self.__access_token is not None) and (time.time() < self.__expire_time):
+            return self.__access_token
+        
+        if self.client_id is None or self.client_secret is None:
+            logger.error("No client id or secret specified for ETIM.")
+            return None
+        timestamp = time.time()
+        url = f"{self.auth_url}/connect/token"
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "scope": self.scope
+        }
+        try:
+            response = requests.post(url, data=data)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            logger.error(f"Authorization at ETIM API failed: {e}")
+            return None
+        self.__expire_time = timestamp + response.json()['expires_in']
+        self.__access_token = response.json()["access_token"]
+        logger.debug(f"Got new access token '{self.__access_token}'. Expires in: {response.json()['expires_in']}[s]")
+        return self.__access_token
     
     @staticmethod
     def parse_class_id(class_id:str) -> str | None:
