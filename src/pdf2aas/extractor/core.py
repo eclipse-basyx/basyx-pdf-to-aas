42714d6d0b00b42e1704a70aab80e46827252e13
Julian Vogel <julian.vogel@sms-group.com>
2024-08-14 11:29:53 +0200

    doc(ext): update doc strings for Property class

--- src/pdf2aas/extractor/core.py
+++ src/pdf2aas/extractor/core.py
@@ -10,9 +10,10 @@ class Property():
     Attributes:
         label (str): The label of the property, e.g. "Rated Torque".
         value (any): The extracted value of the property.
-        unit (str): The measurement unit as found in the data source.
+        unit (str): The measurement unit for the given value.
         reference (str): A reference (~100 chars) where the value was found, e.g. an excerpt, or page reference. 
-        defintion (PropertyDefinition): Definition of the property
+        defintion (PropertyDefinition | None): Definition of the property if available.
+        language(str): Language code (default en) used for the fields (except maybe reference, when it was translated).
     """
     label: str = ""
     value: any = None
@@ -22,11 +23,16 @@ class Property():
     language: str = "en"
 
     @property
-    def definition_id(self):
+    def definition_id(self) -> str | None:
+        """Get the id of the definition if available, else None."""
         return self.definition.id if self.definition is not None else None
     
     @property
-    def definition_name(self):
+    def definition_name(self)  -> str | None:
+        """Get the definition name for the property language.
+           
+           Returns the first definition name if selected language is not available.
+           Returns None if no definition or name is available."""
         if self.definition is None:
             return None
         name = self.definition.name.get(self.language)
