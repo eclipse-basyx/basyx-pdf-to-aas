4cd59b2ba12d2abf425e27079bd2e4f6793b6902
Julian Vogel <julian.vogel@sms-group.com>
2024-09-02 15:20:56 +0200

    test(dict): add simple test for cdd dictionary

--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -1,5 +1,4 @@
-from pdf2aas.dictionary import ECLASS, DummyDictionary, PropertyDefinition
-
+from pdf2aas.dictionary import CDD, ECLASS, DummyDictionary, PropertyDefinition, dictionary_serializer
 
 def test_dummy_dictionary_get_class_properties():
     d = DummyDictionary()
@@ -7,6 +6,51 @@ def test_dummy_dictionary_get_class_properties():
         PropertyDefinition("EF003647", {"en": "Switching distance"}, "numeric")
     ]
 
+class TestCDD:
+    @staticmethod
+    def test_get_class_properties():
+        d = CDD(release="V2.0018.0002")
+        class_id = "0112/2///62683#ACC501#002"
+        properties = d.get_class_properties(class_id)
+        assert len(properties) == 45
+
+        assert class_id in d.classes.keys()
+        class_ = d.classes[class_id]
+        assert class_.id == class_id
+        assert class_.name == "Inductive proximity switch"
+        assert (
+            class_.description
+            == "proximity switch producing an electromagnetic field within a sensing zone for detecting objects and having a semiconductor switching element"
+        )
+        assert class_.keywords == []
+
+        switching_distance = PropertyDefinition(
+            id="0112/2///62683#ACE251#001",
+            name={"en": "rated operating distance"},
+            type="numeric",
+            definition={
+                "en": "conventional quantity used to designate the operating distances"
+            },
+            unit="mm",
+            values=[],
+        )
+        assert switching_distance in properties
+        assert CDD.properties["0112/2///62683#ACE251#001"] == switching_distance
+
+        mounting_position_values = [
+            {
+                'value': 'flush mounting',
+                'id': '0112/2///62683#ACH210#001',
+                'definition': 'embeddable mounting when any damping material can be placed around the sensing face plane without influencing its characteristics',
+                'symbol': 'FLUSHMOUNT'
+            }, {
+                'value': 'not flush mounting',
+                'id': '0112/2///62683#ACH211#001',
+                'definition': 'non-embeddable mounting when a specified free zone around its sensing face is necessary in order to maintain its characteristics.',
+                'symbol': 'NOTFLUSH'
+            }
+        ]
+        assert CDD.properties["0112/2///62683#ACE811#002"].values == mounting_position_values
 
 class TestECLASS:
     @staticmethod
