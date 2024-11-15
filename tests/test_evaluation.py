15d2aecf98308b2d7b0ed3df0f191726cd729376
Julian Vogel <julian.vogel@sms-group.com>
2024-11-15 15:32:06 +0100

    test(eval): add test for evaluation extraction

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,23 +1,49 @@
-import re
 import pytest
+from pathlib import Path
+from unittest.mock import patch
 
 from  pdf2aas.evaluation import EvaluationAAS, EvaluationArticle
 
-class TestEvaluation:
-    ...
-    # TODO test comparision: ignored Properties, case sensitive, ...
+from test_generator import test_property_list2
 
 class TestEvaluationAAS:
 
-    def test_add_articles(self):
-        evaluation = EvaluationAAS()
-        evaluation.add_articles(
+    @classmethod
+    def setup_class(cls):
+        cls.evaluation = EvaluationAAS()
+        cls.evaluation.add_articles(
             aasx_list=["tests/assets/dummy-result-aas-template.aasx"],
             datasheet_list=["tests/assets/dummy-test-datasheet.pdf"]
         )
-        assert len(evaluation.articles) == 1
-        assert evaluation.articles[0].name == "dummy-result-aas-template"
+        with patch('pdf2aas.extractor.PropertyLLM.extract') as mock_extract:
+            mock_extract.return_value = test_property_list2
+            cls.evaluation.run_extraction()
+            mock_extract.assert_called_once()
+            cls.extract_args, _ = mock_extract.call_args
 
+    def test_add_articles(self):
+        assert len(self.evaluation.articles) == 1
+        assert self.evaluation.articles[0].name == "dummy-result-aas-template"
+
+    def test_run_extraction(self):
+        assert self.extract_args[0] == self.evaluation.articles[0].datasheet_text
+        assert self.extract_args[1] == self.evaluation.articles[0].definitions
+
+        assert len(self.evaluation.extracted_properties) == 1
+        extracted_properties = next(iter(self.evaluation.extracted_properties.values()))
+        for property_ in test_property_list2:
+            assert property_ in extracted_properties
+        
+        assert self.evaluation.counts_sum.compared == 3
+        assert self.evaluation.counts_sum.correct == 3
+        assert self.evaluation.counts_sum.different == 0
+        assert self.evaluation.counts_sum.extra == 0
+        assert self.evaluation.counts_sum.extracted == 3
+        assert self.evaluation.counts_sum.ignored == 0
+        assert self.evaluation.counts_sum.similar == 0
+        assert self.evaluation.counts_sum.value == 3
+
+    @staticmethod
     @pytest.mark.parametrize("submodel_id,property_parent,property_selection,expected_property_count", [
         (None, None, None, 9),
         # submodel filter
@@ -35,7 +61,6 @@ class TestEvaluationAAS:
         ("TechnicalData", "GeneralInformation", ["property1"], 1),
     ])
     def test_filter_properties(
-        self,
         submodel_id,
         property_parent,
         property_selection,
@@ -54,7 +79,8 @@ class TestEvaluationAAS:
         assert len(article.definitions) == expected_property_count
         assert len(article.values) == expected_property_count
 
-    def test_fill_eclass_etim_ids(self):
+    @staticmethod
+    def test_fill_eclass_etim_ids():
         evaluation = EvaluationAAS()
         evaluation.datasheet_eclass_pattern = r"ECLASS *([\d.]+): *(\d{2}-\d{2}-\d{2}-\d{2})"
         evaluation.datasheet_etim_pattern = r"ETIM *([\d.]+): *(EC\d{6})"
@@ -76,8 +102,9 @@ ETIM       9.0:    EC002714
             "ECLASS": {"14.0": "27-27-40-01", "13.0": "27-27-40-01",},
             "ETIM": {"9.0": "EC002714",}
         }
-    
-    def test_test_cut_datasheet(self):
+
+    @staticmethod
+    def test_test_cut_datasheet():
         evaluation = EvaluationAAS()
         evaluation.datasheet_cutoff_pattern = "# Heading 2"
         text = evaluation._cut_datasheet(["# Heading 1\ntext", "# Heading 2\ntext"])
